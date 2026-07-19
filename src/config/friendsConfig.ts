41a2222115083fb8ffe2a33683ffdcadb1fd1493
murenyou <2728984538@qq.com>
2026-07-19 21:04:55 +0800

    3

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -76,6 +76,18 @@ export const friendsConfig: FriendLink[] = [
 		weight: 10, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用true
 	},
+
+
+	{
+		title: "番茄主理人",
+		imgurl:
+			"https://q1.qlogo.cn/g?b=qq&nk=20447289&s=640",
+		desc: "坐而言不如起而行.",
+		siteurl: "https://fqzlr.com/",
+		tags: ["Blog"],
+		weight: 10, // 权重，数字越大排序越靠前
+		enabled: true, // 是否启用true
+	},
 ];
 
 // 获取启用的友链并进行排序
